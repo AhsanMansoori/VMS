c23d648e525c335ffc02a478f206a3eb1036ae9f
Ahsan Mansoori <72138346+AhsanMansoori@users.noreply.github.com>
2024-01-01 08:30:44 +0500

    Add files via upload

---
+++ VMS/Inc/head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html? lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <link rel="stylesheet" href="style.css">
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/Jquery.js"></script>
+
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <h1>hello</h1>
+</body>
+</html>
